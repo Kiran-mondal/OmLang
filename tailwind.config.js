c0dddc433be91f4ac6c2048e2831eea070494f08
Kiran Mondal <kiranmondal5516@gmail.com>
2026-07-19 22:08:17 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}", // <-- এই লাইনটি যুক্ত করা হয়েছে
   ],
   theme: {
     extend: {},
